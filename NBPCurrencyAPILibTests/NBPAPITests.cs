05c2786214b4559b603f668c702ba6d6998f0571
KifoPL <kifounaabi@gmail.com>
2021-07-26 20:01:53 +0200

    Added unit tests for all async methods.

--- NBPCurrencyAPILibTests/NBPAPITests.cs
+++ NBPCurrencyAPILibTests/NBPAPITests.cs
@@ -24,15 +24,51 @@ namespace NBPCurrencyAPILib.Tests
         }
 
         [TestMethod()]
+        public void GetCurrencyAsyncAtDateTest()
+        {
+            Assert.IsTrue(NBPAPI.GetCurrencyAsync(NBPAPI.TableCode.A, "EUR", new DateTime(2021, 07, 12)).Result.Contains("code"));
+        }
+
+        [TestMethod()]
         public void GetCurrenciesAsyncTest()
         {
             Assert.IsTrue(NBPAPI.GetCurrenciesAsync(NBPAPI.TableCode.A, "EUR", 3).Result.Contains("code"));
         }
 
         [TestMethod()]
-        public void GetCurrencyAsyncAtDateTest()
+        public void GetCurrenciesAsyncTestFromTo()
         {
-            Assert.IsTrue(NBPAPI.GetCurrencyAsync(NBPAPI.TableCode.A, "EUR", new DateTime(2021, 07, 12)).Result.Contains("code"));
+            Assert.IsTrue(NBPAPI.GetCurrenciesAsync(NBPAPI.TableCode.A, "EUR", new DateTime(2021, 07, 21), new DateTime(2021, 07, 25)).Result.Contains("code"));
+        }
+
+        [TestMethod()]
+        public void GetTableAsyncTest()
+        {
+            Assert.IsTrue(NBPAPI.GetTableAsync(NBPAPI.TableCode.A).Result.Contains("EUR"));
+        }
+
+        [TestMethod()]
+        public void GetTableAsyncTestAtDate()
+        {
+            Assert.IsTrue(NBPAPI.GetTableAsync(NBPAPI.TableCode.A, new DateTime(2021, 07, 20)).Result.Contains("EUR"));
+        }
+
+        [TestMethod()]
+        public void GetTableTodayAsyncTest()
+        {
+            Assert.IsTrue(NBPAPI.GetTableTodayAsync(NBPAPI.TableCode.A).Result.Contains("EUR"));
+        }
+
+        [TestMethod()]
+        public void GetTablesAsyncTest()
+        {
+            Assert.IsTrue(NBPAPI.GetTablesAsync(NBPAPI.TableCode.A, 3).Result.Contains("EUR"));
+        }
+
+        [TestMethod()]
+        public void GetTablesAsyncTestFromTo()
+        {
+            Assert.IsTrue(NBPAPI.GetTablesAsync(NBPAPI.TableCode.A, new DateTime(2021, 07, 20), new DateTime(2021, 07, 26)).Result.Contains("EUR"));
         }
     }
 }
