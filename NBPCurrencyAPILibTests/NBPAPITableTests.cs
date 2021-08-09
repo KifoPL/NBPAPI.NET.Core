e9a35e7686ac39aa3bae95ba7b797faf5a9f32f7
KifoPL <kifounaabi@gmail.com>
2021-08-09 21:28:39 +0200

    Updated tests

--- NBPCurrencyAPILibTests/NBPAPITableTests.cs
+++ NBPCurrencyAPILibTests/NBPAPITableTests.cs
@@ -9,37 +9,8 @@ using System.Threading.Tasks;
 namespace NBPCurrencyAPILib.Tests
 {
     [TestClass()]
-    public class NBPAPITests
+    public class NBPAPITableTests
     {
-        [TestMethod()]
-        public void GetCurrencyAsyncTest()
-        {
-            Assert.IsTrue(NBPAPI.GetCurrencyAsync('A', "EUR").Result.Contains("code"));
-        }
-
-        [TestMethod()]
-        public void GetCurrencyTodayAsyncTest()
-        {
-            Assert.IsTrue(NBPAPI.GetCurrencyTodayAsync('A', "EUR").Result.Contains("code"));
-        }
-
-        [TestMethod()]
-        public void GetCurrencyAsyncAtDateTest()
-        {
-            Assert.IsTrue(NBPAPI.GetCurrencyAsync('A', "EUR", new DateTime(2021, 07, 12)).Result.Contains("code"));
-        }
-
-        [TestMethod()]
-        public void GetCurrenciesAsyncTest()
-        {
-            Assert.IsTrue(NBPAPI.GetCurrenciesAsync('A', "EUR", 3).Result.Contains("code"));
-        }
-
-        [TestMethod()]
-        public void GetCurrenciesAsyncTestFromTo()
-        {
-            Assert.IsTrue(NBPAPI.GetCurrenciesAsync('A', "EUR", new DateTime(2021, 07, 21), new DateTime(2021, 07, 25)).Result.Contains("code"));
-        }
 
         [TestMethod()]
         public void GetTableAsyncTest()
